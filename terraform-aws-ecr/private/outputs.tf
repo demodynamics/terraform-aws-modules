2ba29a9f49c3b189589a0f74b4c7f75db3be88d0
alco <you@example.com>
2025-03-10 18:00:47 +0400

    Init

--- terraform-aws-ecr/private/outputs.tf
+++ terraform-aws-ecr/private/outputs.tf
@@ -1,6 +1,10 @@
 output "private_ecr_data" {
   value = {
-    "Private Repo Names" = aws_ecr_repository.private_repo[*].name
+    "Private Repo Name(s)"      = aws_ecr_repository.private_repo[*].name
     "Image Tag Mutability Type" = aws_ecr_repository.private_repo[*].image_tag_mutability
+    "Private Repo Full Arn"     = aws_ecr_repository.private_repo[*].arn
+    "Private Repo URL"          = aws_ecr_repository.private_repo[*].repository_url
+    "Private Repo Registry ID"  = aws_ecr_repository.private_repo[*].registry_id
+    
   }
 }
--- terraform-aws-ecr/public/outputs.tf
+++ terraform-aws-ecr/public/outputs.tf
@@ -1,5 +1,9 @@
 output "public_ecr_data" {
   value = {
-    "Public Repo name" = aws_ecrpublic_repository.public_repo.repository_name
+    "Public Repo ID"           = aws_ecrpublic_repository.public_repo[*].id
+    "Public Repo Name(s)"         = aws_ecrpublic_repository.public_repo[*].repository_name
+    "Public Repo Full Arn"     = aws_ecrpublic_repository.public_repo[*].arn
+    "Public Repo URL"          = aws_ecrpublic_repository.public_repo[*].repository_uri
+    "Public Repo Registry ID"  = aws_ecrpublic_repository.public_repo[*].registry_id
   }
 }
