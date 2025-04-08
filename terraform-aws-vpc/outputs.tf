786d138f274291d77bbac14b483109443d840904
alco <you@example.com>
2025-04-08 22:59:30 +0400

    Init

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -1,13 +1,13 @@
 output "output_data" {
   description = "VPC Details"
   value = {
-    "VPC ID"                  = aws_vpc.vpc.id
-    "VPC Inetrnet Gateway ID" = aws_internet_gateway.this.id
-    "Subnet ID's"             = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
-    "Public Subnet ID"        = aws_subnet.public[*].id
-    "Public Subenet AZNames"  = aws_subnet.public[*].availability_zone
-    "Private Subnet ID"       = aws_subnet.private[*].id
-    "Private Subenet AZNames" = aws_subnet.private[*].availability_zone
+    vpc_id                 = aws_vpc.vpc.id
+    igw_id                 = aws_internet_gateway.this.id
+    subnet_ids             = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
+    public_subnet_ids      = aws_subnet.public[*].id
+    public_subnet_aznames  = aws_subnet.public[*].availability_zone
+    private_subnet_ids     = aws_subnet.private[*].id
+    private_subnet_aznames = aws_subnet.private[*].availability_zone
   }
 }
 
