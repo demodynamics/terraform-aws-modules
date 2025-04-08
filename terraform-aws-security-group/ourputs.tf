1a36f6c59ef94de10df08e01632d6dac556b0d9d
alco <you@example.com>
2025-04-09 01:08:30 +0400

    Init

--- terraform-aws-security-group/ourputs.tf
+++ terraform-aws-security-group/ourputs.tf
@@ -1,7 +1,7 @@
 output "output_data" {
   value = {
-    id = aws_security_group.this.id
-    arn = aws_security_group.this.arn
+    id   = aws_security_group.this.id
+    arn  = aws_security_group.this.arn
     name = aws_security_group.this.name
   }
 }
