85bdba8228ca3359740b6a3ffcecdb6d7456adb5
alco <you@example.com>
2025-04-08 22:38:35 +0400

    Init

--- terraform-aws-iam/roles/irsa/outputs.tf
+++ terraform-aws-iam/roles/irsa/outputs.tf
@@ -3,6 +3,6 @@ output "output_data" {
     name            = aws_iam_role.irsa.name
     arn             = aws_iam_role.irsa.arn
     namespace       = var.service_account_namespace
-    aervice_account = var.service_account_name
+    service_account = var.service_account_name
   }
 }
