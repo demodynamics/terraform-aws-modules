d6e4eef7dd59c61763a6fb0e40300ea67475a2cc
Karen <demodynamics@proton.me>
2025-04-16 20:29:45 +0400

    Init

--- terraform-aws-ecr/private/outputs.tf
+++ terraform-aws-ecr/private/outputs.tf
@@ -1,10 +1,10 @@
 output "output_data" {
   description = "Private ECR repository's details"
-  value = {
-    name                   = aws_ecr_repository.private.name
-    arn                    = aws_ecr_repository.private.arn
-    url                    = aws_ecr_repository.private.repository_url
-    registryID             = aws_ecr_repository.private.registry_id
-    imageTagMutabilityType = aws_ecr_repository.private.image_tag_mutability
+  value       = {
+    name                      = aws_ecr_repository.private.name
+    arn                       = aws_ecr_repository.private.arn
+    url                       = aws_ecr_repository.private.repository_url
+    registry_id               = aws_ecr_repository.private.registry_id
+    image_tag_mutability_type = aws_ecr_repository.private.image_tag_mutability
   }
 }
