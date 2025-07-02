7d30894ab7ca32abac6e72f1e3297b3cf980b21b
Karen <demodynamics@proton.me>
2025-07-02 23:15:55 +0400

    Init

--- terraform-aws-eks/outputs.tf
+++ terraform-aws-eks/outputs.tf
@@ -1,6 +1,8 @@
 output "output_data" {
   description = "EKS Cluster details "
   value = {
-    name = aws_eks_cluster.this.name
+    name                       = aws_eks_cluster.this.name
+    endpoint                   = aws_eks_cluster.this.endpoint
+    certificate_authority_data = aws_eks_cluster.this.certificate_authority[0].data
   }
 }
