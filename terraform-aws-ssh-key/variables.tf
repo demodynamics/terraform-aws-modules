9b3e88ef007e0ce01f9f63d56514dd3a923f6f50
demodynamics <144003968+demodynamics@users.noreply.github.com>
2025-07-16 17:42:14 +0400

    Update variables.tf

--- terraform-aws-ssh-key/variables.tf
+++ terraform-aws-ssh-key/variables.tf
@@ -15,25 +15,25 @@ variable "tls_private_key_rsa_bits" {
 variable "aws_key_pair_name" {
   description = "Name of the AWS EC2 key pair to create."
   type        = string
-  default     = "eks-ssh-key"
+  default     = "ssh-key"
 
 }
 
-variable "eks_ssh_private_key_filename" {
-  description = "Filename to save the private key. Default is eks-ssh-key.pem."
+variable "ssh_private_key_filename" {
+  description = "Filename to save the private key. Default is ssh-key.pem."
   type        = string
-  default     = "./eks-ssh-key.pem"
+  default     = "./ssh-key.pem"
 
 }
 
-variable "eks_ssh_private_key_file_permission" {
+variable "ssh_private_key_file_permission" {
   description = "File permission for the private key file. Default is 0400."
   type        = string
   default     = "0400"
 
 }
 
-variable "eks_ssh_private_key_directory_permission" {
+variable "ssh_private_key_directory_permission" {
   description = "Directory permission for the private key file. Default is 0700."
   type        = string
   default     = "0700"
