a705f128f2f88eb4a0bceb28f0b9c4589e52aad2
demodynamics <144003968+demodynamics@users.noreply.github.com>
2025-07-16 18:59:45 +0400

    Update variables.tf

--- terraform-aws-ssh-key/variables.tf
+++ terraform-aws-ssh-key/variables.tf
@@ -5,37 +5,59 @@ variable "tls_private_key_algorithm" {
 
 }
 
-variable "tls_private_key_bits" {
-  description = "Number of bits for the key. Default is 4096."
+variable "tls_private_key_rsa_bits" {
+  description = "Number of bits for the RSA key. Default is 4096."
   type        = number
   default     = 4096
 
 }
 
-variable "aws_ssh_key_pair_name" {
+variable "aws_key_pair_name" {
   description = "Name of the AWS EC2 key pair to create."
   type        = string
-  default     = "ssh-key"
+  default     = "ec2-ssh-key"
 
 }
 
-variable "ssh_private_key_filename" {
-  description = "Filename to save the private key. Default is ssh-key.pem."
+variable "key_name_suffix" {
+  description = "Optional suffix for key rotation/versioning, e.g. -2025-07"
   type        = string
-  default     = "./ssh-key.pem"
+  default     = ""
+}
+
+variable "write_local_file" {
+  type    = bool
+  default = true
+}
+
+variable "eks_ssh_private_key_filename" {
+  description = "Filename to save the private key. Default is eks-ssh-key.pem."
+  type        = string
+  default     = "./ec2-ssh-key.pem"
 
 }
 
-variable "ssh_private_key_file_permission" {
+variable "eks_ssh_private_key_file_permission" {
   description = "File permission for the private key file. Default is 0400."
   type        = string
   default     = "0400"
 
 }
 
-variable "ssh_private_key_directory_permission" {
+variable "eks_ssh_private_key_directory_permission" {
   description = "Directory permission for the private key file. Default is 0700."
   type        = string
   default     = "0700"
 
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Owner       = "karen"
+    CreatedBy   = "terraform"
+    Project     = "ansible-test"
+    Environment = "dev"
+    ManagedBy   = "terraform"
+  }
 }
