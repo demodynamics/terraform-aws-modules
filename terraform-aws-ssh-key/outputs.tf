e849ee3d32c9b3b540356bf1f32a1b726264c946
demodynamics <144003968+demodynamics@users.noreply.github.com>
2025-07-16 18:59:29 +0400

    Update outputs.tf

--- terraform-aws-ssh-key/outputs.tf
+++ terraform-aws-ssh-key/outputs.tf
@@ -1,16 +1,15 @@
-output "ssh_private_key_pem" {
-  description = "The private key for SSH access to EC2 Instance or EKS nodes. Save this securely!"
-  value       = tls_private_key.ssh.private_key_pem
-  sensitive   = true
-}
-
-output "key_name" {
+output "ssh_key_name" {
   description = "The name of the AWS EC2 key pair"
   value       = aws_key_pair.ssh.key_name
 }
 
-output "private_key_pem" {
-  description = "The private key in PEM format"
+output "ssh_private_key_pem" {
+  description = "The private key in PEM format for SSH access to EC2 Instances. Save this securely!"
   value       = tls_private_key.ssh.private_key_pem
   sensitive   = true
+}
+
+output "ssh_public_key_openssh" {
+  value     = tls_private_key.ssh.public_key_openssh
+  sensitive = false
 }
