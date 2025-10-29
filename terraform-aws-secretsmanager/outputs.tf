9a6e21f44e5acc317340d32313491b7e6842bc8e
Demo Dynamics <demodynamics@proton.me>
2025-10-29 13:11:30 +0400

    Chnages

--- terraform-aws-secretsmanager/outputs.tf
+++ terraform-aws-secretsmanager/outputs.tf
@@ -7,3 +7,9 @@ output "secret_name" {
   description = "The Name of the AWS Secrets Manager secret."
   value       = aws_secretsmanager_secret.secret.name
 }
+
+output "secret_version_id" {
+  description = "The Version ID of the AWS Secrets Manager secret."
+  value       = aws_secretsmanager_secret_version.secret_version.version_id
+
+}
